9b236bdc8e224ac29727b14e4d3e3b7f4112cdc2
Shu Miyao <info@befive.info>
2019-11-12 20:55:23 +0000

    module.exports error fix

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,12 @@
 import timberCore from './_tm.core.js';
 
 // Core instance
-module.exports = new timberCore({
-    initialize: false
-});
+module.exports = initTimberCore;
+
+function initTimberCore(options = { initialize: false }) {
+    if (!(this instanceof initTimberCore)) {
+        return new timberCore(options)
+    } else {
+        return false;
+    }
+}
