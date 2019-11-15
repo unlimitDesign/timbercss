0f28734e9d674cd08f1c6c6064930c173fcd7d4a
Shu Miyao <info@befive.info>
2019-11-15 17:13:22 +0000

    lib location clean up

--- npm-scripts/webpack.updateTimberLibs.js
+++ npm-scripts/webpack.updateTimberLibs.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const $TimberTools = require('./lib/TimberTools.js');
+const $TimberTools = require('./lib/TimberTools_library.js');
 
 const Tbs = new $TimberTools({ env: 'updateTimberLibs', outputDir: 'dist' });
 
