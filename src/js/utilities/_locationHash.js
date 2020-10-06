d177de2d198e65f21664469462db267e2c10af55
Christian Lundgren <udfrance@gmail.com>
2020-10-06 21:59:41 +0200

    Fixed hashtag detection bug for accordion and tab component, improved hash detection

--- src/js/utilities/_locationHash.js
+++ src/js/utilities/_locationHash.js
@@ -2,10 +2,17 @@ const locationHash = () =>{
   
   if (document == 'undefined' || window == 'undefined') return false;
 
-  const url = window.location.hash, id = url.indexOf('#');
-  const hash = id != -1 ? url.substring(id).replace('/','') : '';
-  if(hash !== '' && document.querySelectorAll(hash).length){
-    return [true, hash];
+  const url = window.location.hash;
+  const hash = url.indexOf('#');
+  const id = hash != -1 ? url.substring(hash).replace('/','') : '';
+  const justHash = id.substring(1) === '' ? true : false;
+
+  if(!justHash){
+    if(id !== '' && document.querySelectorAll(id).length){
+      return [true, id];
+    }else{
+      return false;
+    }
   }else{
     return false;
   }
